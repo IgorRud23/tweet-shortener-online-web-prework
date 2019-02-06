98cf74fc02a50b98fbc0bac25b4a87366b4299d3
Igor Rud <rud.igor23@gmail.com>
2019-02-06 01:01:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+
+def dictionary
+words = {"hello" => "hi",
+"to" => "2",
+"two" => "2",
+"too" => "2",
+"for" => "4",
+"four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@",
+"and" => "&"}
+end
+
+def word_substituter(tweet_one)
+  tweet_one.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+tweets.map do |phrase|
+  puts word_substituter(phrase)
+end
+end
+
+def selective_tweet_shortener(tweet_two)
+  tweet_two.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length < 130
+      phrase
+end
+end
+end
